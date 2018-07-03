a067212bc23ca7dd1ac2d6a11eb31ff9c9a29a8b
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-03 09:48:16 -0500

    initial commit

---
+++ src/main/java/com/lab2/demo/GreetingController.java
@@ -0,0 +1,17 @@
+package com.lab2.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @Value("${message}")
+    private String message;
+
+    @GetMapping("/")
+    public String greeting() {
+        return message;
+    }
+}
